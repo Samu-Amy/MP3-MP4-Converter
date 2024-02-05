8257fcda86aa60a95a8941a2cbb51ef663ff0400
Samu-Amy <amisanosamuele@gmail.com>
2024-02-05 22:05:53 +0100

    some fixes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 import pytube.request
+import time
 
 
 # Settings
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
-pytube.request.default_range_size = 1024 * 1024 * 560 # BYTE
+# pytube.request.default_range_size = 1024 * 0.5 # BYTE
 
 # TODO: (opzioni (es. output path) salvate in un file di testo?)
 
@@ -21,34 +22,44 @@ window.title("MP3 Downloader")
 # Variables
 url = tkinter.StringVar()
 chunk_sizes = ["0.25MB", "0.5MB", "1MB", "9MB"]
+start = None
 
 
 # Functions
 def updateChunkSize(size):
-    chunk_size = 1024 * 1024 * float(size[0:-2])
-    pytube.request.default_range_size = chunk_size
+    chunk_size = 1024 * float(size[0:-2])
+    # pytube.request.default_range_size = chunk_size
+    print(pytube.request.default_range_size)
 
-def singleDownload():
+def download():
     download_finish_label.configure(text="")
     download_finish_label.update()
-    try:
-        yt_link = download_link_entry.get()
-        yt_object = YouTube(yt_link, on_progress_callback=singleProgress)
-        audio = yt_object.streams.get_audio_only()
-        audio.download(output_path="G:\\") # TODO: rendi settabile
-        download_finish_label.configure(text=f"Downloaded - {yt_object.title}")
-    except:
-        download_finish_label.configure(text="Invalid link")
+    # TODO: riattiva try and catch
+    # try:
+    yt_link = download_link_entry.get()
+    yt_object = YouTube(yt_link, on_progress_callback=downloadProgress, on_complete_callback=downloadCompleted)
+    audio = yt_object.streams.get_audio_only()
+    start = time.time()
+    audio.download(output_path="G:\\")
+    download_finish_label.configure(text=f"Downloaded - {yt_object.title}")
+    # except:
+    #     download_finish_label.configure(text="Invalid link")
+    download_finish_label.update()
 
-def singleProgress(stream, chunk, bytes_remaining):
+def downloadProgress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     percentage = bytes_downloaded / total_size * 100
-    print(percentage)
-    single_progress_percentage.configure(text=f"{int(percentage)} %")
+    single_progress_percentage.configure(text=f"{percentage:.2f} %")
     single_progress_percentage.update()
     single_progress_bar.set(percentage/100)
     # single_progress_bar.update()
+
+def downloadCompleted():
+    print("end")
+    end = time.time()
+    duration_label.configure(text=f"Last download duration: {start - end}s")
+    duration_label.update()
 
 
 # UI Elements
@@ -82,7 +93,7 @@ single_progress_percentage = customtkinter.CTkLabel(main_frame, text="0%")
 single_progress_bar = customtkinter.CTkProgressBar(main_frame, width=400)
 single_progress_bar.set(0)
 
-single_download_button = customtkinter.CTkButton(main_frame, text="Download", command=singleDownload)
+single_download_button = customtkinter.CTkButton(main_frame, text="Download", command=download)
 
 
 # Layout
@@ -109,6 +120,7 @@ download_link_entry.pack()
 download_finish_label.pack()
 single_progress_percentage.pack()
 single_progress_bar.pack()
+single_download_button.pack()
 
 
 # Run App
